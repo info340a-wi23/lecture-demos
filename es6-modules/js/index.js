8c9626c4b3afda042bbedf27f5e24d2117a2c324
Tim Carlson <timca@arenasports.net>
2023-02-06 15:11:39 -0800

    pre-react-intro and es6 modules lecture checkin

---
+++ es6-modules/js/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// //Slide 7
+// import './other.js'
+
+// // Slide 7a
+// import {otherMessage} from './other.js';
+
+// //Slide 8
+// import {otherMessage, otherFunc} from './other.js';
+
+// //Slide 10
+// import {otherMessage} from './other.js';
+// import otherDefault from './other.js';
+
+
+//code executing
+
+// //Slide 6 - see it's working in liveserver
+// console.log("We're live from index.js");
+
+// //Slide 7
+// console.log("Other says: ", otherMessage);
+
+
+// //Slide 8
+// console.log("Other says: ", otherMessage);
+// otherFunc();
+
+// //Slide 10
+// console.log("Other says: ", otherMessage);
+// otherDefault();
+
