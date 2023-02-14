f3eea132e25270e8b9029d6d916d6e0f297329f2
Tim Carlson <timca@arenasports.net>
2023-02-13 17:19:16 -0800

    post react-chat-1 lecture

--- react-chat-1/src/components/App.js
+++ react-chat-1/src/components/App.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { HeaderBar } from './HeaderBar';
+import { ChannelList } from './ChannelList'
+import { ChatPane } from './ChatPane';
 
-function App(props) {
+// 
+// function App(props) {
+//   return (
+//     <div>Hello App</div>
+//   );
+// }
+
+// export default function App(props) {
+//   return (
+//     <div>
+//       <HeaderBar />
+//       <ChannelList />
+//       <ChatPane/>
+//     </div>
+//   );
+// }
+
+
+// export default function App(props) {
+
+//   return (
+//     <div>
+
+//       <HeaderBar />
+//       <div className='row'>
+//         <div className='col-2'>
+//           <ChannelList />
+//         </div>
+//         <main className='col'>
+//           <ChatPane />
+//         </main>
+//       </div>
+//     </div>
+//   )
+// };
+
+
+export default function App(props) {
+  const currentChannel = 'general';
+
   return (
-    <div>Hello App</div>
-  );
-}
+    <div className='d-flex flex-column'>
 
-export default App;
+      <HeaderBar />
+      <div className='row flex-grow-1'>
+        <div className='col-3'>
+          <ChannelList currentChannel={currentChannel} />
+        </div>
+        <div className='col-8'>
+          <ChatPane currentChannel={currentChannel}/>
+        </div>
+      </div>
+    </div>
+  )
+};
---
+++ react-chat-1/src/components/ChatPane.js
@@ -0,0 +1,196 @@
+import React from 'react';
+
+// export function ChatPane(props) {
+//     return (
+//         <div>
+//             <MessageItem />;
+//             <MessageItem />;
+//             <MessageItem />;
+//             <MessageItem />;
+//         </div>
+//     )
+// }
+
+// function MessageItem(props) {
+
+//     return (
+//         <div className='message'>
+//             <img src='/img/Parrot.png' alt='parrot avatar' />
+//             <p>Parrot</p>
+//             <p>Squawk Squawk</p>
+//         </div>
+//     )
+// }
+
+// export function ChatPane(props) {
+//     return (
+//         <div>
+//             <MessageItem />;
+//             <MessageItem />;
+//             <MessageItem />;
+//             <MessageItem />;
+//         </div>
+//     )
+// }
+
+// function MessageItem(props) {
+
+//     return (
+//         <div className='message d-flex'>
+//             <div className='me-2'>
+//                 <img src='/img/Parrot.png' alt='parrot avatar' />
+//             </div>
+//             <div className='flex-grow-1'>
+//                 <p className='user-name'>Parrot</p>
+//                 <p>Squawk Squawk</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// import CHAT_HISTORY from '../data/chat_log.json';
+
+// export function ChatPane(props) {
+   
+//     return (
+//         <div>
+//              <MessageItem messageData={CHAT_HISTORY[0]}/>
+//              <MessageItem messageData={CHAT_HISTORY[1]}/>
+//              <MessageItem messageData={CHAT_HISTORY[2]}/>
+//              <MessageItem messageData={CHAT_HISTORY[3]}/>
+//         </div>
+//     )
+// }
+
+// function MessageItem(props) {
+//     const userName = props.messageData.userName;
+//     const userImg = props.messageData.userImg;
+//     const text = props.messageData.text;
+
+//     return (
+//         <div className='message d-flex'>
+//             <div className='me-2'>
+//                 <img src={userImg} alt={userName+ " avatar"} />
+//             </div>
+//             <div className='flex-grow-1'>
+//                 <p className='user-name'>{userName}</p>
+//                 <p>{text}</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// import CHAT_HISTORY from '../data/chat_log.json';
+
+// export function ChatPane(props) {
+
+//     const messageItemArray = CHAT_HISTORY.map((messageObj) => {
+//         const element = <MessageItem messageData={messageObj} key={messageObj.timestamp} />
+//         return element;
+// })
+//     return (
+//         <div className='pt-2'>
+//         {messageItemArray}  
+//         </div>     
+//     )
+// }
+
+// function MessageItem(props) {
+//     const { userName, userImg, text} = props.messageData
+
+//     return (
+//         <div className='message d-flex mb-2'>
+//             <div className='me-2'>
+//                 <img src={userImg} alt={userName+ " avatar"} />
+//             </div>
+//             <div className='flex-grow-1'>
+//                 <p className='user-name'>{userName}</p>
+//                 <p>{text}</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// import CHAT_HISTORY from '../data/chat_log.json';
+
+// export function ChatPane(props) {
+//     const currentChannel='random';
+
+//     // const currentChannel = props.currentChannel;
+//     const channelMessage = CHAT_HISTORY.filter((msgObj) => {
+//         //if current channel, then keep
+//         return (msgObj.channel === currentChannel)
+//     })
+    
+//     const messageItemArray = channelMessage.map((messageObj) => {
+//         const element = <MessageItem messageData={messageObj} key={messageObj.timestamp}/>
+//         return element;
+// })
+
+//     return (
+//         <div className='pt-2'>
+//         {messageItemArray}  
+//         </div>     
+//     )
+// }
+
+// function MessageItem(props) {
+
+//     const { userName, userImg, text} = props.messageData
+
+//     return (
+//         <div className='message d-flex mb-2'>
+//             <div className='me-2'>
+//                 <img src={userImg} alt="Parrot's profile" />
+//             </div>
+//             <div className='flex-grow-1'>
+//                 <p className='user-name'>{userName}</p>
+//                 <p>{text}</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+import CHAT_HISTORY from '../data/chat_log.json';
+
+export function ChatPane(props) {
+
+    const currentChannel = props.currentChannel;
+    const channelMessage = CHAT_HISTORY.filter((msgObj) => {
+        //if current channel, then keep
+        return (msgObj.channel === currentChannel)
+    })
+    
+    const messageItemArray = channelMessage.map((messageObj) => {
+        const element = <MessageItem messageData={messageObj} key={messageObj.timestamp}/>
+        return element;
+})
+
+    return (
+        <div className='pt-2'>
+        {messageItemArray}  
+        </div>     
+    )
+}
+
+function MessageItem(props) {
+
+    const { userName, userImg, text} = props.messageData
+
+    const handleClick = (event) => {
+        console.log("you clicked on", userName);
+    }
+
+    return (
+        <div className='message d-flex mb-2'>
+            <div className='me-2'>
+                <img src={userImg} alt="Parrot's profile" onClick={handleClick} />
+            </div>
+            <div className='flex-grow-1'>
+                <p className='user-name'>{userName}</p>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}
