69882bbd6aa76e3aaa9aeeacf59564954c4e747b
Tim Carlson <timca@arenasports.net>
2023-02-22 20:39:48 -0800

    post lecture

--- react-chat-router/src/index.js
+++ react-chat-router/src/index.js
@@ -1,20 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-//import CSS
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-
-import App from './components/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-// // Slide 16
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-
-// import { BrowserRouter } from 'react-router-dom';
 
 // //import CSS
 // import 'bootstrap/dist/css/bootstrap.css';
@@ -23,11 +8,26 @@ root.render(<App />);
 // import App from './components/App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// // root.render(<App />);
+// root.render(<App />);
 
-// // Slide 16
-// root.render(
-//     <BrowserRouter>
-//         <App />
-//     </BrowserRouter>
-// );
+// Slide 16
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from 'react-router-dom';
+
+//import CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+// Slide 16
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
